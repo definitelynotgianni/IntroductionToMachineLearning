c9fad2afc594a8ab9ed19e7416967dd67df176db
definitelynotgianni <gianniorigami@gmail.com>
2022-12-11 00:38:01 +0100

    Added scatterplot and nice colors

--- HW_01/Exercise_1.py
+++ HW_01/Exercise_1.py
@@ -5,18 +5,28 @@
 
 #Setup
 import numpy as np 
+import matplotlib.pyplot as plt
 np.random.seed(42)
+pointCount = 500
 
 #function for drawing s random numbers from normal distribution
 def RandNorm( s ):
     return np.random.normal( size = s )
 
 #create two lists filled with random floats drawn from a normal distribution 
-X = RandNorm(500)
-Y = RandNorm(500) 
+X = RandNorm(pointCount)
+Y = RandNorm(pointCount) 
 
 #create arry by putting in the two lists created earlier and transpose it to get the right shape
 D = np.array([X,Y])
 np.transpose(D)
 
-print(np.shape(D)) #REMOVE BEFORE SUBMITTING! ONLY FOR TEST PURPOSES
+
+
+
+for i in np.arange(1, pointCount):
+    
+    plt.scatter(X[:i], Y[:i], s = 20, c = (np.sqrt(X[:i]**2 + Y[:i]**2 )))
+    
+
+plt.show()
